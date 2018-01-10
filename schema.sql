88fa76c97bcc48b5f210771462fc95fb14c20cb8
Alex Choi <wanchoi211@users.noreply.github.com>
2018-01-10 14:19:36 -0800

    changed user_auth and lowercased all letters;

--- schema.sql
+++ schema.sql
@@ -4,8 +4,17 @@ CREATE DATABASE food_swap;
 
 \connect food_swap;
 
+
+CREATE TABLE users_auth (
+  id SERIAL,
+  password VARCHAR(256) NOT NULL,
+  salt VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE users (
   id SERIAL,
+  user_auth_id INTEGER NOT NULL REFERENCES users_auth(id),
   name VARCHAR(25) NOT  NULL,
   username VARCHAR(25) NOT NULL,
   picture VARCHAR(255) NULL DEFAULT NULL,
@@ -16,17 +25,9 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE users_auth (
-  id SERIAL,
-  user_Id INTEGER NOT NULL REFERENCES users(id),
-  password VARCHAR(256) NOT NULL,
-  salt VARCHAR(256) NOT NULL,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE food (
   id SERIAL,
-  user_Id INTEGER NOT NULL REFERENCES users(id),
+  user_id INTEGER NOT NULL REFERENCES users(id),
   dishName VARCHAR(25) NULL DEFAULT NULL,
   description VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (id)
@@ -34,17 +35,17 @@ CREATE TABLE food (
 
 CREATE TABLE friends (
   id SERIAL,
-  user_Id1 INTEGER NOT NULL REFERENCES users(id),
-  user_Id2 INTEGER NOT NULL REFERENCES users(id),
+  user_id1 INTEGER NOT NULL REFERENCES users(id),
+  user_id2 INTEGER NOT NULL REFERENCES users(id),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE trade (
   id SERIAL,
-  user_Id1 INTEGER NOT NULL REFERENCES users(id),
-  food_Id1 INTEGER NOT NULL REFERENCES food(id),
-  user_Id2 INTEGER NOT NULL REFERENCES users(id),
-  food_Id2 INTEGER NOT NULL REFERENCES food(id),
+  user_id1 INTEGER NOT NULL REFERENCES users(id),
+  food_id1 INTEGER NOT NULL REFERENCES food(id),
+  user_id2 INTEGER NOT NULL REFERENCES users(id),
+  food_id2 INTEGER NOT NULL REFERENCES food(id),
   status BOOLEAN DEFAULT NULL,
   PRIMARY KEY (id)
 );
