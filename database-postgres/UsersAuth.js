9eca0a7f645eaf46bec2c48857fbc3289eb45ec3
Alex Choi <wanchoi211@gmail.com>
2018-01-10 19:20:00 -0800

    usersAuthId changed to userAuthId

--- database-postgres/UsersAuth.js
+++ database-postgres/UsersAuth.js
@@ -6,14 +6,14 @@ class UsersAuth {
     return db.any(queryString, [password, salt]);
   }
 
-  static updatePassword({ usersAuthId, password, salt }) {
+  static updatePassword({ userAuthId, password, salt }) {
     const queryString = 'UPDATE users_auth SET password = $2, salt = $3 WHERE id  = $1';
-    return db.any(queryString, [usersAuthId, password, salt]);
+    return db.any(queryString, [userAuthId, password, salt]);
   }
 
-  static findByUserId({ usersAuthId }) {
+  static findByUserAuthId({ userAuthId }) {
     const queryString = 'SELECT * FROM users_auth WHERE id = $1 LIMIT 1';
-    return db.any(queryString, [usersAuthId])
+    return db.any(queryString, [userAuthId])
       .then(data => data[0]);
   }
 }
