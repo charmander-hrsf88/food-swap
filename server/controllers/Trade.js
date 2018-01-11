d3fd62adce4a0a68ff7cfc5e6205d17bbc7f444c
Alex Choi <wanchoi211@gmail.com>
2018-01-11 07:40:52 -0800

    Base code written for api calls

--- server/controllers/Trade.js
+++ server/controllers/Trade.js
@@ -1,5 +1,15 @@
 class Trade {
+  static getAll(req, res) {
+    res.end('got all the trades');
+  }
 
+  static getById(req, res) {
+    res.end('got a trade with id');
+  }
+
+  static post(req, res) {
+    res.end('posted a new trade');
+  }
 }
 
 exports = Trade;
