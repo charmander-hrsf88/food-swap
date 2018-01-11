feebad771b6d6ae6ad91413e3fd5e42e9783bb46
Alex Choi <wanchoi211@gmail.com>
2018-01-10 19:11:15 -0800

    Trade.js success and failure implemeted

--- database-postgres/Trade.js
+++ database-postgres/Trade.js
@@ -5,6 +5,16 @@ class Trade {
     const queryString = 'INSERT INTO trade (user_id1, food_id1, user_id2, food_id2) VALUES ($1, $2, $3, $4)';
     return db.any(queryString, [userId1, foodId1, userId2, foodId2]);
   }
+
+  static success({ id }) {
+    const querySting = 'UPDATE trade SET status = 1 WHERE id = $1';
+    return db.any(queryString, [id]);
+  }
+
+  static failure({ id }) {
+    const queryString = 'UDATE trade SET status = 0 Where id = $1';
+    return db.any(queryString, [id]);
+  }
 }
 
 exports = Trade;
