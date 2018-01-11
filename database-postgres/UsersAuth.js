499ed8a333a76043a225430c998e383e038a82ea
Alex Choi <wanchoi211@users.noreply.github.com>
2018-01-10 16:05:49 -0800

    Update UsersAuth.js

--- database-postgres/UsersAuth.js
+++ database-postgres/UsersAuth.js
@@ -1,20 +1,21 @@
 const db = require('./config');
 
 class UsersAuth {
-  createUser(userId, password, salt) {
-    const queryString = 'INSERT INTO users_auth (userId, password, salt) VALUES ($1, $2, $3)';
-    return db.any(queryString, [userId, password, salt]);
+  createUser({ password, salt }) {
+    const queryString = 'INSERT INTO users_auth (password, salt) VALUES ($1, $2)';
+
+    return db.any(queryString, [password, salt]);
   }
 
-  updatePassword(userId, newPassword) {
-    const queryString = 'UPDATE users_auth SET password = $2 WHERE userId = $1';
-    return db.any(queryString, [userId, newPassword])
+  updatePassword({usersAuthId, password, salt}) {
+    const queryString = 'UPDATE users_auth SET password = $2, salt = $3 WHERE id  = $1';
+    return db.any(queryString, [usersAuthId, password, salt]);
   }
 
-  findByUserId(userId) {
-    const queryString = 'SELECT * FROM users_auth WHERE userId = $1 LIMIT 1';
-    return db.any(queryString, [userId])
-              .then(data => data[0])
+  findByUserId({usersAuthId}) {
+    const queryString = 'SELECT * FROM users_auth WHERE id = $1 LIMIT 1';
+    return db.any(queryString, [usersAuthId])
+              .then(data => data[0]);
   }
 }
 
