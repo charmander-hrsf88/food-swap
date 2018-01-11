b0bb3460992922a848b26db7b5c05804eda1b6e4
Alex Choi <wanchoi211@gmail.com>
2018-01-10 16:34:01 -0800

    UserAuth.js linted

--- database-postgres/UsersAuth.js
+++ database-postgres/UsersAuth.js
@@ -2,20 +2,19 @@ const db = require('./config');
 
 class UsersAuth {
   createUser({ password, salt }) {
-    const queryString = 'INSERT INTO users_auth (password, salt) VALUES ($1, $2)';
-
-    return db.any(queryString, [password, salt]);
+    this.queryString = 'INSERT INTO users_auth (password, salt) VALUES ($1, $2)';
+    return db.any(this.queryString, [password, salt]);
   }
 
-  updatePassword({usersAuthId, password, salt}) {
-    const queryString = 'UPDATE users_auth SET password = $2, salt = $3 WHERE id  = $1';
-    return db.any(queryString, [usersAuthId, password, salt]);
+  updatePassword({ usersAuthId, password, salt }) {
+    this.queryString = 'UPDATE users_auth SET password = $2, salt = $3 WHERE id  = $1';
+    return db.any(this.queryString, [usersAuthId, password, salt]);
   }
 
-  findByUserId({usersAuthId}) {
-    const queryString = 'SELECT * FROM users_auth WHERE id = $1 LIMIT 1';
-    return db.any(queryString, [usersAuthId])
-              .then(data => data[0]);
+  findByUserId({ usersAuthId }) {
+    this.queryString = 'SELECT * FROM users_auth WHERE id = $1 LIMIT 1';
+    return db.any(this.queryString, [usersAuthId])
+      .then(data => data[0]);
   }
 }
 
