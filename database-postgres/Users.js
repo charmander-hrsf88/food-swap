24cac4c7497618fb0fae7f5dfb12cbbfdd3f6ef8
Alex Choi <wanchoi211@users.noreply.github.com>
2018-01-10 16:03:35 -0800

    Update Users.js

--- database-postgres/Users.js
+++ database-postgres/Users.js
@@ -2,21 +2,25 @@ const db = require('./config');
 const usersAuth = require('./UsersAuth');
 
 class Users {
-  create({ name, username, email }) {
-    const queryString = 'INSERT INTO users (name, username, email) VALUES ($1, $2, $3)';
-    return db.any(queryString, [name, username, email]);
+  create({ userAuthId, name, username, email }) {
+    const queryString = 'INSERT INTO users (user_auth_id, name, username, email) VALUES ($1, $2, $3, $4)';
+    return db.any(queryString, [userAuthId, name, username, email]);
   }
 
   findById({ id }) {
     const queryString = 'SELECT * FROM users WHERE id = $1 LIMIT 1';
-    return db.any(queryString, [id])
+    return db.any(queryString, [id]).then(users => users.length === 0 ? null : users[0]);
+  }
+
+  findByAuthId({ userAuthId }) {
+    const queryString = 'SELECT * FROM users WHERE user_auth_id = $1 LIMIT 1';
+    return db.any(queryString, [userAuthId]).then(users => users.length === 0 ? null : users[0]);
   }
 
   findByUsername({ username }) {
     const queryString = 'SELECT * FROM users WHERE username = $1 LIMIT 1';
-    return db.any(queryString, [username])
+    return db.any(queryString, [username]).then(users => users.length === 0 ? null : users[0]);
   }
 }
 
-const user = new Users();
-user.create({ name: 'alex', username: 'wanchoi211', email: 'wanchoi211@gmail.com' }).then(() => user.findById(1)).then(user => console.log(user));
+module.exports = new Users();
