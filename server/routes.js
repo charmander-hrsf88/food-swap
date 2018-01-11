97840e16cbc98170cba1d30f88f35f31daa8351f
Alex Choi <wanchoi211@gmail.com>
2018-01-11 07:13:14 -0800

    API proposed

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,20 @@
 const controller = require('./controllers');
 const router = require('express').Router();
 
+router.get('/api/users', controller.users.getAll);
+router.get('/api/users/:id', controller.users.getById);
+router.get('/api/users', controller.users.post);
 
-router.get('/login', controller.login.get);
-router.post('/login', controller.login.post);
+router.get('/api/trade', controller.trade.getAll);
+router.get('/api/trade/:id', controller.trade.getById);
+router.post('/api/trade', controller.trade.post);
 
-router.get('/signup', controller.signup.get);
-router.post('/signup', controller.signup.post);
+router.get('/api/food', controller.food.getAll);
+router.get('/api/food/:id', controller.food.getById);
+router.post('/api/food', controller.food.post);
+
+router.get('/api/friends', controller.friends.getAll);
+router.get('/api/friends/:id', controller.friends.getById);
+router.post('/api/friends', controller.friends.post);
+
+exports = router;
