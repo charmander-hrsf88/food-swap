23d9c89278c95d2d9cb1728b4c735de817a3e65d
Hayden Marx <haydenmarx@gmail.com>
2018-01-12 15:32:05 -0800

    Update trade view

--- react-client/src/components/trades.jsx
+++ react-client/src/components/trades.jsx
@@ -1,17 +1,75 @@
 import React from 'react';
 import axios from 'axios';
 import Trade from './trade.jsx';
+import NavBar from './navBar.jsx';
 // import NavBar from './navBar.jsx';
 // import Friends from './friends.jsx';
 // import AddFriends from './addFriends.jsx';
 // import TopUsers from './topUsers.jsx';
 // import dummyData from '../dummyData.js'
 
+/*
+  id SERIAL,
+  user_id1 INTEGER NOT NULL REFERENCES users(id),
+  food_id1 INTEGER NOT NULL REFERENCES food(id),
+  user_id2 INTEGER NOT NULL REFERENCES users(id),
+  food_id2 INTEGER NOT NULL REFERENCES food(id),
+  status BOOLEAN DEFAULT NULL,
+  PRIMARY KEY (id) */
+
 class Trades extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      trades: [{}, {}, {}],
+      trades: [
+        {
+          username1: 'Hayden',
+          food_id1: 'Cookie Crisp',
+          username2: 'Alex',
+          food_id2: 'Cheerios',
+          status: null,
+        },
+        {
+          username1: 'Hayden',
+          food_id1: 'Cookie Crisp',
+          username2: 'Alex',
+          food_id2: 'Cheerios',
+          status: true,
+        },
+        {
+          username1: 'Hayden',
+          food_id1: 'Cookie Crisp',
+          username2: 'Alex',
+          food_id2: 'Cheerios',
+          status: false,
+        },
+        {
+          username1: 'Alex',
+          food_id1: 'Cookie Crisp',
+          username2: 'Hayden',
+          food_id2: 'Cheerios',
+          status: null,
+        },
+        {
+          username1: 'Alex',
+          food_id1: 'Cookie Crisp',
+          username2: 'Hayden',
+          food_id2: 'Cheerios',
+          status: true,
+        },
+        {
+          username1: 'Alex',
+          food_id1: 'Cookie Crisp',
+          username2: 'Hayden',
+          food_id2: 'Cheerios',
+          status: false,
+        },
+      ],
+      user: {
+        id: 1,
+        username: 'Hayden',
+        name: 'Hayden',
+      },
       // friends: dummyData.friends,
       // topUsers: dummyData.topUsers,
     };
@@ -45,10 +103,16 @@ class Trades extends React.Component {
 
   render() {
     return (
-      <ol>
-        {this.state.trades.map((trade, index) =>
-          <Trade key={index} trade={trade} />)}
-      </ol>
+      <div>
+        <NavBar />
+        <div id="trades">
+          <h2>Trades</h2>
+          <ol>
+            {this.state.trades.map((trade, index) =>
+              <Trade key={index} trade={trade} user={this.state.user} />)}
+          </ol>
+        </div>
+      </div>
     );
   }
 }
