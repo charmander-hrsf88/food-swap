750cf1ef922744a89c0d1db59d8c726b0844184d
Hayden Marx <haydenmarx@gmail.com>
2018-01-11 18:01:23 -0800

    Rebasing

--- react-client/src/components/router.jsx
+++ react-client/src/components/router.jsx
@@ -6,12 +6,16 @@ import {
 } from 'react-router-dom';
 import App from './app.jsx';
 import LogIn from '../log-in-page/logIn.jsx';
+import Profile from './profile.jsx';
+import Trades from './trades.jsx'
 
 const Switcher = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/login" component={LogIn} />
+      <Route path="/user/:userName" component={Profile} />
+      <Route path="/trades" component={Trades} />
     </Switch>
   </Router>
 );
